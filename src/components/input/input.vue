ce33e3f6aebae06862862165d36c6fa1093c06e8
xukaiming <xukaiming@Yulintu.com>
2018-03-19 16:26:38 +0800

    增加文本框功能

--- src/components/input/input.vue
+++ src/components/input/input.vue
@@ -2,12 +2,24 @@
     <div :class="wrapClasses">
         <input
             :type="type"
+            :class="inputClasses"
+            :placeholder="placeholder"
+            :name="name"
+            :maxlength="maxlength"
+            :autofocus="autofocus"
+            :disabled="disabled"
+            :readonly="readonly"
+            :value="currentValue"
+            @change="handleChange"
+            @focus="handleFocus"
+            @blur="handleBlur"
         />
     </div>
 </template>
 <script>
     import { oneOf } from '../../utils/tool'
     const prefixCls = 'wxVueYlt-input'
+
     export default {
         name: 'ylt-Input',
         props: {
@@ -16,7 +28,35 @@
                     return oneOf(value, ['text', 'textarea', 'password', 'url', 'email', 'date']);
                 },
                 default: 'text'
-            }
+            },
+            placeholder: {
+                type: String,
+                default: ''
+            },
+            readonly: {
+                type: Boolean,
+                default: false
+            },
+            disabled: {
+                type: Boolean,
+                default: false
+            },
+            maxlength: {
+                type: Number
+            },
+            name: {
+                type: String
+            },
+            autofocus: {
+                type: Boolean,
+                default: false
+            },
+            icon: String
+        },
+        data () {
+           return {
+               currentValue: this.value
+           }
         },
         computed: {
             wrapClasses () {
@@ -26,6 +66,25 @@
                         [`${prefixCls}_${this.type}`]: this.type
                     }
                 ]
+            },
+            inputClasses () {
+                return [
+                    `${prefixCls}`,
+                    {
+                        [`${prefixCls}-disabled`]: this.disabled
+                    }
+                ]
+            }
+        },
+        methods: {
+            handleChange(event) {
+                this.$emit('on-change', event)
+            },
+            handleFocus(event) {
+                this.$emit('on-focus', event)
+            },
+            handleBlur(event) {
+                this.$emit('on-blur', event)
             }
         }
     }
